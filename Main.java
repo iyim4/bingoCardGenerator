db004b7e18eb3bebb1409526b5a7f8450515b782
iyim4 <iyim@utexas.edu>
2024-06-04 14:48:16 -0500

    Add comments and narrrow scope of some variables

--- Main.java
+++ Main.java
@@ -7,37 +7,37 @@ import java.io.IOException;
 import java.lang.Math;
 
 public class Main {
-    private static int longestWordLen; // for card formatting
-    
     public static void main(String[] args) throws IOException {
         Scanner keyboard = new Scanner(System.in);
         ArrayList<String> list = new ArrayList<>();
 
-        // welcome and setup
         System.out.println("Welcome to the bingo card generator! ");
-        getSourceFile(keyboard, list);
-        // length of longest is used for formating, and only in runprogram. consider changing location or smthn.
-        int width = (list.size() >= 25) ? 5 : (int) Math.sqrt(list.size());
-        runProgram(keyboard, width, true, list);
+        int longestWordLen = getSourceFile(keyboard, list);
+        runMenu(keyboard, list, longestWordLen);
         keyboard.close();
     }
 
-    // fills list with words and sets lengthOfLongestWord.
-    private static void getSourceFile(Scanner keyboard, ArrayList<String> list) {
+    /**
+     * Fills {@code list} with words from a file
+     * @param keyboard for user input. Must not be null.
+     * @param list the list to be filled. Previous content will be cleared. Must not be null.
+     * @return the length of the longest word in the file
+     */
+    private static int getSourceFile(Scanner keyboard, ArrayList<String> list) {
         if (!list.isEmpty()) {
             list.clear();
         }
-        
         boolean done = true;
+        int longest = -1;
+
         do {
-            System.out.println("What is the is the data file name? (Note: it must be in the same " +
+            System.out.println("What is the is the data file name? (Note: it must be within the same " +
                 "directory as this program)\nOptions:");
             listFiles();
             System.out.print("Enter a file name: ");
-            File file = new File("./" + keyboard.nextLine());
+            File file = new File(".\\" + keyboard.nextLine());
 
             // attempt to read file
-            int longest = -1;
             try {
                 Scanner scanner = new Scanner(file);
                 while (scanner.hasNextLine()) {
@@ -47,7 +47,6 @@ public class Main {
                         longest = temp.length();
                     }
                 }
-                longestWordLen = longest;
                 scanner.close();
 
                 // repeat if file was empty
@@ -59,26 +58,34 @@ public class Main {
                 }
             } catch (FileNotFoundException error) {
                 System.out.println("Error: File not found.");
-                error.printStackTrace(); //delete
                 done = false;
             }
         } while (!done);
         System.out.println("File found sucessfully. There are " + list.size() + " words.");
+        return longest;
     }
 
-    private static void runProgram(Scanner keyboard, int width, boolean freespace, ArrayList<String> list) {
+    /**
+     * Displays and runs menu options
+     * @param keyboard for user input. Must not be null.
+     * @param list list of words for the card. Must not be null.
+     */
+    private static void runMenu(Scanner keyboard, ArrayList<String> list, int longestWordLen) {
         boolean quit = false;
+        boolean freespace = true; // initial include freespace
+        int width = (list.size() >= 25) ? 5 : (int) Math.sqrt(list.size());  // initial width. default 5 if enough words.
         do {
             System.out.println("\nWould you like to generate a bingo card with a width of " + width +
                 ", and " + ((freespace) ? "a freespace?" : "NO freespace?"));
-                String menuOptions = "\t1. Yes, generate card\t\n2. No, change width\t\n3. No, change freespace\t\n4. No, change data file\n5. Quit";
+                String menuOptions = "\t1. Yes, generate card\n\t2. No, change width\n\t3. No, " +
+                    "change freespace\n\t4. No, change data file\n\t5. Quit";
             int input = getIntFromUser(keyboard, 1, 5, menuOptions);
             System.out.println();
 
             switch (input) {
                 case 1:
                     // generate card
-                    printCard(list, width, freespace, (width * width / 2));
+                    printCard(list, width, freespace, longestWordLen);
                     break;
                 case 2:
                     // change width
@@ -86,7 +93,7 @@ public class Main {
                     width = getIntFromUser(keyboard, 1, maxWidth, "Enter a width, max " + maxWidth);
                     break;
                 case 3:
-                    // change freespace
+                    // toggle freespace
                     freespace = !freespace;
                     System.out.println("Freespace " + ((freespace) ? "added" : "removed"));
                     break;
@@ -98,14 +105,20 @@ public class Main {
                 case 5:
                     // quit
                     quit = true;
-                default: 
-                    //error message
                     break;
             }
         } while (!quit);
         System.out.println("Thank you!");
     }
 
+    /**
+     * Promts user for an integer
+     * @param keyboard for user input. Must not be null.
+     * @param low lower bound, inclusive
+     * @param high upper bound, inclusive
+     * @param message instructions for the user
+     * @return an integer between {@code low} and {@code high}, inclusive.
+     */
     private static int getIntFromUser(Scanner keyboard, int low, int high, String message) {
         boolean done = true;
         int result = 0;
@@ -113,31 +126,41 @@ public class Main {
             System.out.println(message);
             System.out.print("Enter a number between " + low + " and " + high + ": ");
             String input = keyboard.nextLine();
+
+            // validate
             try {
                 result = Integer.parseInt(input);
                 if (result > high || result < low) {
-                    System.out.println("Invalid. Out of range");
+                    System.out.println("Invalid. Out of range.");
                     done = false;
                 } else {
                     done = true;
                 }
             } catch (NumberFormatException error) {
-                System.out.println("Invalid. Not a number");
+                System.out.println("Invalid. Not a number.");
                 done = false;
             }
         } while (!done);
         return result;
     }
 
-    // generates and prints card
-    private static void printCard(ArrayList<String> list, int width, boolean freespace, int freespaceIndex) {
+    /**
+     * Generates and prints the bingo card
+     * @param list list of words for the card. Must not be null.
+     * @param width width of bingo card. Must be less than the square root of list.size().
+     * @param freespace whether to include a FREE space on the card.
+     * @param longestWordLen length of the longest word in the card, for formatting
+     */
+    private static void printCard(ArrayList<String> list, int width, boolean freespace, int longestWordLen) {
         Random random = new Random();
-        int totNumWords = width * width;
+        int numBingoWords = width * width;
+        int freespaceIndex = width * width / 2;
         int endIndex = list.size() - 1;
+        
         // place unique values at end of list and generate random numbers within a decreasingly 
-        // smaller range, guaranteeing unique values every time without the need to reset the 
-        // array for generating new cards.
-        for (int i = 0; i < totNumWords; i++) {
+        // smaller range, guaranteeing unique values every funnction call without the need to reset  
+        // the array for generating new cards. Worst case O(sqrt(N)) card generation.
+        for (int i = 0; i < numBingoWords; i++) {
             int index = random.nextInt(0, endIndex + 1);
             // swap the random word with the word at the end of the working list
             String temp = list.get(index);
@@ -147,13 +170,13 @@ public class Main {
             endIndex--;
         }
 
-        // print out last totNumElems words of list
+        // print out last numBingoWords words of list
         StringBuilder result = new StringBuilder();
-        for (int i = 0; i < totNumWords; i++) {
+        for (int i = 0; i < numBingoWords; i++) {
             if (i % width == 0) result.append('\n');
             String format = "%1$-" + (longestWordLen + 1) + "s";
             String word = list.get(list.size() - 1 - i);
-            if (i == freespaceIndex && freespace) {
+            if (freespace && i == freespaceIndex) {
                 word = "FREE";
             }
             result.append(String.format(format, word));
@@ -162,7 +185,9 @@ public class Main {
         System.out.println(result.toString());
     }
 
-    // lists files in this directory
+    /**
+     * Lists files in this directory
+     */
     private static void listFiles() {
         String[] files = new File(".").list();
         for (String file : files) {
